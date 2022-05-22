b4c822101705800b6ae8a56b32bbdffd85cfe8be
home <chujkova@mail.ru>
2022-05-22 17:23:35 +0500

    2. Анализ доступности сервера. [#859 #303775]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,43 +1,38 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            try (PrintWriter out = new PrintWriter(
+            PrintWriter out = new PrintWriter(
                     new BufferedOutputStream(
                             new FileOutputStream(target)
-                    ))) {
-                var str = in.lines().collect(Collectors.toList());
-                boolean work = true;
-                String time = "";
-                for (var s : str) {
-                    var ss = s.split(" ");
-                    int code = Integer.parseInt(ss[0]);
-                    if (work && (code >= 400 && code < 600)) {
-                        time = ss[1];
-                        work = false;
-                    }
-                    if (!work && !(code >= 400 && code < 600)) {
-                        time = time + ";" + ss[1];
-                        work = true;
-                        out.println(time);
-                    }
+                    ));
+            boolean work = true;
+            String time = "";
+            String s;
+            while ((s = in.readLine()) != null) {
+                var ss = s.split(" ");
+                int code = Integer.parseInt(ss[0]);
+                if (work && (code >= 400 && code < 600)) {
+                    time = ss[1];
+                    work = false;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+                if (!work && !(code >= 400 && code < 600)) {
+                    time = time + ";" + ss[1];
+                    work = true;
+                    out.println(time);
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        var source  = "./data/server.log";
+        var source = "./data/server.log";
         var target = "./data/target.log";
         var analizy = new Analizy();
         analizy.unavailable(source, target);
