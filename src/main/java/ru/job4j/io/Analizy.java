93bf9d9e031f7c41e4e1d0b93a30ab53dc8378f5
home <chujkova@mail.ru>
2022-05-22 16:31:28 +0500

    2. Анализ доступности сервера. [#859 #303775]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,34 +7,31 @@ import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            var str = in.lines().collect(Collectors.toList());
-            boolean work = true;
-            String time = "";
-            for (var s : str) {
-                var ss = s.split(" ");
-                int code = Integer.parseInt(ss[0]);
-                if (work && (code >= 400 && code < 600)) {
-                    time = ss[1];
-                    work = false;
+            try (PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)
+                    ))) {
+                var str = in.lines().collect(Collectors.toList());
+                boolean work = true;
+                String time = "";
+                for (var s : str) {
+                    var ss = s.split(" ");
+                    int code = Integer.parseInt(ss[0]);
+                    if (work && (code >= 400 && code < 600)) {
+                        time = ss[1];
+                        work = false;
+                    }
+                    if (!work && !(code >= 400 && code < 600)) {
+                        time = time + ";" + ss[1];
+                        work = true;
+                        out.println(time);
+                    }
                 }
-                if (!work && !(code >= 400 && code < 600)) {
-                    time = time + ";" + ss[1];
-                    work = true;
-                    rsl.add(time);
-                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            rsl.stream().forEach(out::println);
-        } catch (Exception e) {
             e.printStackTrace();
         }
     }
