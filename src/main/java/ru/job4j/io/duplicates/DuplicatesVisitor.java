b251120b164b4376f73720e84f8e18e02800d6c9
home <chujkova@mail.ru>
2022-05-26 20:32:04 +0500

    4.2. Поиск дубликатов [#315066 #305123]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,22 +6,37 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    HashSet<FileProperty> files = new HashSet<>();
+    HashMap<FileProperty, List<Path>> files = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 
         File file1 = new File(file.toString());
         FileProperty fileProperty = new FileProperty(file1.length(), file1.getName());
-        if (!files.contains(fileProperty)) {
-            files.add(fileProperty);
+        if (files.containsKey(fileProperty)) {
+            files.get(fileProperty).add(file.toAbsolutePath());
         } else {
-            System.out.println(fileProperty.getName());
+            files.put(fileProperty, new ArrayList<>());
+            files.get(fileProperty).add(file.toAbsolutePath());
         }
+
         return super.visitFile(file, attrs);
+    }
+
+    public void printDublicate() {
+        for (var f : files.keySet()) {
+            if (files.get(f).size() > 1) {
+                for (var f1 : files.get(f)) {
+                    System.out.println(f1);
+                }
+            }
+        }
     }
 }
