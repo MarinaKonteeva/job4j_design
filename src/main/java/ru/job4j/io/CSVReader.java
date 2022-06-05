269cce5c187db946fbff04d4aceb983769e9dfef
home <chujkova@mail.ru>
2022-06-05 21:05:19 +0500

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,65 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CSVReader {
+
+    public static void val(String path, String delimiter, String out, Set<String> filter) {
+
+        File file = new File(path);
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("первый аргумент должен быть путь до папки поиска");
+        }
+        if (!";".equals(delimiter)) {
+            throw new IllegalArgumentException("второй аргумент должен быть расширение файла");
+        }
+        file = new File(out);
+        if (!(file.isFile() || "stdout".equals(out))) {
+            throw new IllegalArgumentException("третий аргумент должен быть путь до папки поиска");
+        }
+        if (filter.size() == 0) {
+            throw new IllegalArgumentException("нет полей");
+        }
+    }
+
+    public static void handle(ArgsName argsName) throws Exception {
+        var path = argsName.get("path");
+        var delimiter = argsName.get("delimiter");
+        var out = argsName.get("out");
+        var filter = Arrays.stream(argsName.get("filter").split(",")).collect(Collectors.toSet());
+        val(path, delimiter, out, filter);
+
+        try (var scanner = new Scanner(new FileReader(path));
+             PrintWriter pw = new PrintWriter(new FileWriter(out))) {
+            var titles = scanner.nextLine().split(delimiter);
+            List<String[]> data = new ArrayList<>();
+            while (scanner.hasNext()) {
+                data.add(scanner.nextLine().split(delimiter));
+            }
+
+            String str = "";
+            for (int i = 0; i < titles.length; i++) {
+                if (filter.contains(titles[i])) {
+                    str += str.length() == 0 ? titles[i] : delimiter + titles[i];
+                }
+            }
+            pw.println(str);
+
+            for (var d : data) {
+                str = "";
+                for (int i = 0; i < titles.length; i++) {
+                    if (filter.contains(titles[i])) {
+                        str += str.length() == 0 ? d[i] : delimiter + d[i];
+                    }
+                }
+                pw.println(str);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
