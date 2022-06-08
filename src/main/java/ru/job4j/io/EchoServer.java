7f37f06aa53b5a3238866293afa93db52d969d8f
home <chujkova@mail.ru>
2022-06-08 22:24:58 +0500

    3. Бот [#7921 #310467]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,21 +12,18 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String request = "";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
                         request += str;
                     }
                     if (request.contains("?msg=Bye")) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         System.out.println("выход");
                         server.close();
                     } else if (request.contains("?msg=Hello")) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         out.write("Hello.\r\n".getBytes());
                     } else {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         out.write("What.\r\n".getBytes());
                     }
                     out.flush();
