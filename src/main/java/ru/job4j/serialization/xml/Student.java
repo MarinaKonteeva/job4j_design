358f30dd85ce405c98a48557cea5c89f7ff276c7
home <chujkova@mail.ru>
2022-06-16 20:37:22 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

---
+++ src/main/java/ru/job4j/serialization/xml/Student.java
@@ -0,0 +1,74 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "student")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Student {
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+
+    private String[] statuses;
+
+    public Student() { }
+
+    public Student(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException {
+
+        final Student student = new Student(true, 39, new Contact("11-111"),
+                "Worker", "Married");
+
+        /* Получаем контекст для доступа к АПИ */
+        JAXBContext context = JAXBContext.newInstance(Student.class);
+        /* Создаем сериализатор */
+        Marshaller marshaller = context.createMarshaller();
+        /* Указываем, что нам нужно форматирование */
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            /* Сериализуем */
+            marshaller.marshal(student, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (Exception e) {
+
+        }
+        /* Для десериализации нам нужно создать десериализатор */
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            /* десериализуем */
+            Student result = (Student) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+    }
+
+
+}
