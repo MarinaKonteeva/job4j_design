b6e1237be2131ed884a9d5c1c7fc2701cf7b20a7
home <chujkova@mail.ru>
2022-06-17 18:29:55 +0500

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Student.java
+++ src/main/java/ru/job4j/serialization/json/Student.java
@@ -1,8 +1,13 @@
 package ru.job4j.serialization.json;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Student {
     private final boolean sex;
@@ -49,5 +54,38 @@ public class Student {
                         + "}";
         final Student studentMod = gson.fromJson(studentJson, Student.class);
         System.out.println(studentMod);
+
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Student student1 = new Student(false, 39, new Contact("22-111"),
+                new String[] {"Worker", "Married"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", student1.isSex());
+        jsonObject.put("age", student1.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(student1).toString());
     }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }
